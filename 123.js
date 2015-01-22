36f37f7944218346fa8257872d3d13b7ab9176b7
cloudcome <cloudcome@163.com>
2015-01-22 19:58:26 +0800

    idea

---
+++ 123.js
@@ -0,0 +1,21 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-01-22 19:47
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/123
+     */
+    'use strict';
+
+    var abc = require('./abc.js');
+
+    if(1){
+
+    }
+
+    module.exports = {};
+});
